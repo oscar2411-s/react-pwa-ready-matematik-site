4a50c70888b15bb8d511a97a0b49d1639a580173
Olle <57236774+oscar2411-s@users.noreply.github.com>
2021-02-03 07:29:27 +0100

    Some small changes

--- public/service-worker.js
+++ public/service-worker.js
@@ -2,14 +2,13 @@ var doCache = true;
 
 var CACHE_NAME = "my-pwa-cache-v1";
 
-self.addEventListener("activate", event => {
+self.addEventListener("activate", (event) => {
   const cacheWhitelist = [CACHE_NAME];
   event.waitUntil(
-    caches.keys().then(keyList =>
+    caches.keys().then((keyList) =>
       Promise.all(
-        keyList.map(key => {
+        keyList.map((key) => {
           if (!cacheWhitelist.includes(key)) {
-            console.log("Deleting cache: " + key);
             return caches.delete(key);
           }
         })
@@ -18,28 +17,27 @@ self.addEventListener("activate", event => {
   );
 });
 
-self.addEventListener("install", function(event) {
+self.addEventListener("install", function (event) {
   if (doCache) {
     event.waitUntil(
-      caches.open(CACHE_NAME).then(function(cache) {
+      caches.open(CACHE_NAME).then(function (cache) {
         fetch("manifest.json")
-          .then(response => {
+          .then((response) => {
             response.json();
           })
-          .then(assets => {
+          .then((assets) => {
             const urlsToCache = ["/", assets["main.js"]];
             cache.addAll(urlsToCache);
-            console.log("cached");
           });
       })
     );
   }
 });
 
-self.addEventListener("fetch", function(event) {
+self.addEventListener("fetch", function (event) {
   if (doCache) {
     event.respondWith(
-      caches.match(event.request).then(function(response) {
+      caches.match(event.request).then(function (response) {
         return response || fetch(event.request);
       })
     );
